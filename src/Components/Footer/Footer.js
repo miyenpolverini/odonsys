87e809c84437edb98dffcaea067b16e3b2cac476
miyen <miyenpolverini@gmail.com>
2024-06-09 23:50:16 -0300

    third version

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return <div className='containerFooter'>
         <div className='barra-div'></div>
         <div className='footerName'>
-            <p className='footer'>Copyright © 2022. Miyén Polverini.</p>
+            <p className='footer'>Copyright © 2024. Miyén Polverini.</p>
             <p className='footer'>Todos los derechos reservados.</p>
         </div>
     </div>;
